94c87346b07e1e71b677840d05bdd53b11ca60c6
tarek-23 <tarek.23@aucegypt.edu>
2021-02-18 09:55:42 +0200

    Setup: adds the homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home.js";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Router>
+        <Switch>
+          {/* Home */}
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          {/* Search */}
+          <Route path="/search">
+            <h1>Search page</h1>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
